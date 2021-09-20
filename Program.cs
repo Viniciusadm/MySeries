98401b7292351ad68b846dae4344a9d8ec45528d
Viniciusadm <viniciusam.22@gmail.com>
2021-09-20 09:55:50 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
